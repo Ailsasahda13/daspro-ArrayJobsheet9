277702964b9e1b636140e56f5b77fb19d3fca665
Ailsasahda13 <ailsashda13@gmail.com>
2024-11-15 10:50:43 +0700

    Jobsheet 9

---
+++ ArrayValue244107060006.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ArrayValue244107060006 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] finalScore = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter the final score " + i + ": ");
+            finalScore[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("final score " + i + " is " + finalScore[i]);
+
+}
+}
+}
+
